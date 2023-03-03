3a4378af0a4f4dc5ddc033bf92054dc97a26ab05
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2023-03-04 01:07:53 +0530

    resume component first section done

--- src/components/ComponentToPrint.jsx
+++ src/components/ComponentToPrint.jsx
@@ -1,37 +1,36 @@
 import React from "react";
-import { tceheader } from "../assets";
+import tceheader from "../assets/tce-header.png";
 import { useSelector } from "react-redux";
 export const ComponentToPrint = React.forwardRef((props, ref) => {
-  const userData=useSelector((state)=>state.userData)
+  const userData = useSelector((state) => state.userData)
   return (
-    <div  className="flex justify-center mt-20 mx-auto  items-center">
-<div  className="h-[297mm] overflow-y-scroll shadow-2xl border-zinc-400 border-4 break-after-page  w-[210mm] m-0 p-0">
-    <div ref={ref} className="bg-white pt-6 " >
-    <div className="mr-12"><img src={tceheader} alt="tceheader"></img></div>
+    <div className="flex justify-center mt-20 mx-auto  items-center">
+      <div className="h-[297mm] overflow-y-scroll shadow-2xl border-zinc-400 border-4 break-after-page  w-[210mm] m-0 p-0">
+        <div ref={ref} className="bg-white" >
+          <div className="w-full"><img src={tceheader} alt="tceheader"></img></div>
+          <div className="absolute right-[2cm] ">
+            {userData.profilePhoto !== "" && <img src={userData.profilePhoto}  className="h-[4.5cm] w-[3.5cm]" alt="profilephoto"></img>}
+          </div>
+          <div className="text-[12pt] mt-6 mx-[2cm]" >
+            <div><span className="font-bold">Name :</span> {userData.name}</div>
+            <div><span className="font-bold">Reg No :</span> {userData.regno}</div>
+            <div><span className="font-bold">Degree :</span> {userData.educationList[1].degree}{' '}{userData.educationList[1].field}</div>
 
-    <div className="flex  p-6  justify-center items-center" >
-      <div className="text-lg"><span className="font-bold">Name :</span> {userData.name}</div>
-      <img src={userData.profilePhoto} width="100" alt="profilephoto"></img>
+            <div className="mt-6 ">
+              <span className="font-bold">PROFESSIONAL OBJECTIVE:</span>
+              <div>{userData.professionalObjective}</div>
+            </div>
 
-    </div>
-    <div className="text-2xl p-6 break-after-page">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-    <div className="text-2xl p-6 ">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-    <div className="text-2xl  p-6">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint oc
-    </div>
-    <div className="text-2xl p-6 ">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-    <div className="text-2xl p-6 ">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-</div>
-    </div>
+            <div className="mt-6">
+              <span className="font-bold">ACADEMIC RECORD:</span>
+              <div></div>
+            </div>
+
+
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 });
